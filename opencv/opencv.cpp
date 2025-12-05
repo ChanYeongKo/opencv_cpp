52ac9fcbf394e57af61adad788e18c3240a9cbf8
RTX <RTX@>
2025-12-05 17:41:02 +0900

    초기 커밋.

---
+++ opencv/opencv.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/opencv.hpp"
+#include "iostream"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat img = imread("lenna.bmp");
+	imshow("img)", img);
+	waitKey();
+	return 0;
+}
