9ebc00a43aa0acf5057da894645204911aea423c
고찬영 <118610396+ChanYeongKo@users.noreply.github.com>
2025-12-16 22:11:52 +0900

    Add files via upload

---
+++ chap3-2/chap3-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+//과제 1
+/*
+int main() {
+	
+	int b, g, r;
+	cout << "Blue값(0~255)을 입력하시오: ";
+	cin >> b;
+	cout << "Green값(0~255)을 입력하시오: ";
+	cin >> g;
+	cout << "Red값(0~255)을 입력하시오: ";
+	cin >> r;
+	Vec3b color(b, g, r);
+	cout << "입력한 컬러값은 " << color << "입니다." << endl;
+	return 0;
+
+}
+*/
+
+//과제 2
+/*
+int main() {
+	int b, g, r;
+	cout << "Blue값(0~255)을 입력하시오: ";
+	cin >> b;
+	cout << "Green값(0~255)을 입력하시오: ";
+	cin >> g;
+	cout << "Red값(0~255)을 입력하시오: ";
+	cin >> r;
+	Scalar color(b, g, r);
+	cout << "입력한 컬러값은 " << color << "입니다." << endl;
+	return 0;
+	
+}
+*/
